c9224d8f73d0df77f50f99ba62fe61ac982feaac
Kashyap297 <kashyap29700@gmail.com>
2024-09-27 22:06:31 +0530

    Quiz api created successfully

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,9 +3,9 @@ const dbConnection = require("./config/db");
 const Config = require("./config");
 const cors = require("cors");
 const path = require("path");
-
+const quizRoutes = require("./routes/quizRoutes");
 const app = express();
-
+const errorHandler = require("./middleware/errorHandler");
 const PORT = Config.PORT || 5000;
 
 // Middleware to parse incoming JSON
@@ -15,11 +15,13 @@ app.use(cors());
 
 // Serve static files from the uploads directory
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use(errorHandler);
 
 // dbConnection
 dbConnection();
 
-// api routes
+// Routes
+app.use("/api", quizRoutes);
 
 app.listen(PORT, (err) => {
   if (err) {
