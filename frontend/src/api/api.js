3305b20ebd94b10a7e3fec376d4fb7941f7cb97c
Kashyap297 <kashyap29700@gmail.com>
2024-09-28 00:11:43 +0530

    api integration

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,11 +2,11 @@ import axios from "axios";
 
 // Create an Axios instance with the base URL
 const api = axios.create({
-  baseURL: "http://localhost:8000/api", // Replace with your backend URL
+  baseURL: "https://swiftrut-task-10.onrender.com/api", // Replace with your backend URL
 });
 
 // Export baseURL for use in other components
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://swiftrut-task-10.onrender.com";
 
 // Add a request interceptor to include token in requests (if necessary)
 api.interceptors.request.use((config) => {
